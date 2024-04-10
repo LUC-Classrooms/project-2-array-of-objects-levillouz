39e986eef71c4072227f049a918bef2bcfcae676
Lev Illouz <156964356+levillouz@users.noreply.github.com>
2024-04-10 22:15:26 +0000

    Needs fixing

--- script.js
+++ script.js
@@ -10,25 +10,50 @@
  * Use the draw() function to display and move your objects independently on the canvas.
 ***/
 
-var obj1, obj2// Global Variables go here
-var objects = new Array(10); // the argument to Array() defines its size
+// Global Variables go here
+var gnome1, gnome2
+
 
 function setup(){
-  createCanvas(320, 240);
-  obj1 = new MyClass(100, 100);
-  obj2 = new MyClass(200, 100);
-  for(var i = 0; i < objects.length; i++) {
-    objects[i] = new MyClass(random(width), random(height));
-    }
-  }
+  createCanvas(600, 400);
+  gnome1 = new Gnome(width/4, height/2);
+  console.log(gnome1);
+  gnome2 = new Gnome(width * 3/4, height/2);
+  console.log (gnome2);
+
 }
+
 function draw(){
   background(200);
-  for(var i = 0; i < objects.length; i++){
-    objects[i].move(); // each time through the loop, move the next object in the array
-    objects[i].display(); // call the display method for each object (0 - 9)
-  }
+  gnome1.display();
+  gnome2.display();
+  gnome1.move();
+  gnome2.move();
 
+
+
+function Gnome (tempX, tempY) {
+this.x = tempX;
+this.y = tempY;
+this.r = 0;
+this.s = 1;
+this.rSpeed = random (-.1, .1);
+this.xSpeed = random (-4, 4);
+this.ySpeed = random (-4, 4);
+}
+this.move = function(){
+  this.x += this.xSpeed;
+  if (this.x > width || this.x = 0) {
+    this.xSpeed *= -1;
+  }
+this.y += this.ySpeed;
+if(this.y > height || this.y < 0) {
+this.ySpeed *= -1;
+  }
+}
+this.display = function() {
+push ()
+  translate (this.x, this.y);
   // Draw gnome
   fill(0, 0, 153); // Body
   ellipse(0, 120, 150, 200);
@@ -46,11 +71,8 @@ function draw(){
   rect(-50, -80, 100, 50, 20);
   fill(255, 0, 0);
   rect(-30, -130, 60, 70, 20);
-  
-  obj1.move(); // move object 1 (obj1)
-  obj1.display(); // display obj1 on screen
-  obj2.move();
-  obj2.display();
+pop ()
+}
 }
 
 
