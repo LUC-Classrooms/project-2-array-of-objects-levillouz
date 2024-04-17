84c21ecd95909cd208baf8f55e6c7ebe5dc7e4f0
Lev Illouz <156964356+levillouz@users.noreply.github.com>
2024-04-17 19:25:49 +0000

    Project 2 Completed

--- script.js
+++ script.js
@@ -11,11 +11,15 @@
 ***/
 
 // Global Variables go here
+var gnomes = new Array (10);
 var gnome1, gnome2
 
 
 function setup(){
   createCanvas(600, 400);
+  for(let i = 0; i < gnomes.length; i++){
+    gnomes[i] = new Gnome(width/2, height/2);
+  }
   gnome1 = new Gnome(width/4, height/2);
   console.log(gnome1);
   gnome2 = new Gnome(width * 3/4, height/2);
@@ -25,54 +29,60 @@ function setup(){
 
 function draw(){
   background(200);
-  gnome1.display();
-  gnome2.display();
+  //gnome1.display();
+  //gnome2.display();
   gnome1.move();
   gnome2.move();
 
+for(let i = 0; i < gnomes.length; i++){
+  gnomes[i].display();
+gnomes[i].move();
+}
+
+}
 
 
 function Gnome (tempX, tempY) {
-this.x = tempX;
-this.y = tempY;
-this.r = 0;
-this.s = 1;
-this.rSpeed = random (-.1, .1);
-this.xSpeed = random (-4, 4);
-this.ySpeed = random (-4, 4);
-}
-this.move = function(){
-  this.x += this.xSpeed;
-  if (this.x > width || this.x = 0) {
-    this.xSpeed *= -1;
+  this.x = tempX;
+  this.y = tempY;
+  this.r = 0;
+  this.s = 1;
+  this.rSpeed = random (-.1, .1);
+  this.xSpeed = random (-4, 4);
+  this.ySpeed = random (-4, 4);
+  
+  this.move = function(){
+    this.x += this.xSpeed;
+    if (this.x > width || this.x < 0) {
+      this.xSpeed *= -1;
+    }
+    this.y += this.ySpeed;
+    if(this.y > height || this.y < 0) {
+      this.ySpeed *= -1;
+    }
   }
-this.y += this.ySpeed;
-if(this.y > height || this.y < 0) {
-this.ySpeed *= -1;
+  this.display = function() {
+    push ()
+    translate (this.x, this.y);
+    // Draw gnome
+    fill(0, 0, 153); // Body
+    ellipse(0, 120, 150, 200);
+    fill(255, 204, 153); // Head
+    ellipse(0, 0, 100, 120);
+    fill(0); // Eyes
+    ellipse(-30, -20, 20, 20);
+    ellipse(30, -20, 20, 20);
+    fill(255, 102, 102); // Nose
+    triangle(0, 0, -10, 20, 10, 20);
+    noFill(); // Mouth
+    stroke(255, 102, 102);
+    arc(0, 20, 40, 20, 0, PI);
+    fill(0); // Hat
+    rect(-50, -80, 100, 50, 20);
+    fill(255, 0, 0);
+    rect(-30, -130, 60, 70, 20);
+    pop ()
   }
-}
-this.display = function() {
-push ()
-  translate (this.x, this.y);
-  // Draw gnome
-  fill(0, 0, 153); // Body
-  ellipse(0, 120, 150, 200);
-  fill(255, 204, 153); // Head
-  ellipse(0, 0, 100, 120);
-  fill(0); // Eyes
-  ellipse(-30, -20, 20, 20);
-  ellipse(30, -20, 20, 20);
-  fill(255, 102, 102); // Nose
-  triangle(0, 0, -10, 20, 10, 20);
-  noFill(); // Mouth
-  stroke(255, 102, 102);
-  arc(0, 20, 40, 20, 0, PI);
-  fill(0); // Hat
-  rect(-50, -80, 100, 50, 20);
-  fill(255, 0, 0);
-  rect(-30, -130, 60, 70, 20);
-pop ()
-}
 }
 
 
